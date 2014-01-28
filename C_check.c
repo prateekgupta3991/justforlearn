725718967f2239e80e7b4d641979e4057fd9f58c
Prateek Gupta <prateekgupta.3991@gmail.com>
2014-01-28 22:50:43 +0530

    eigth commit

---
+++ C_check.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	printf("Yoyo commit");
+	printf("now its working but still havent tried the usability of init");
+	printf("fuck yoyo");
+	printf("yoyo bitches.....work done successfully!!");
+	printf("now need to work on git submodule");
+}
